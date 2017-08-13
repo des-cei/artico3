ebd0f253b9b2f3cff2f0083a969b776a78a3feaf
Alfonso Rodríguez Medina <alfonsorm89@gmail.com>
2017-08-13 18:10:08 +0000

    addvector.cpp edited online with Bitbucket

--- demos/addvector/src/a3_addvector/hls/addvector.cpp
+++ demos/addvector/src/a3_addvector/hls/addvector.cpp
@@ -1,1 +1,15 @@
-// TODO
+#include "artico3.h"
+
+#define VALUES (1024)
+
+A3_KERNEL(a3in_t a, a3in_t b, a3out_t c){
+
+	unsigned int i;
+
+//	for (i = 0; i < MEMPOS; i++) {
+//	for (i = 0; i < values/2; i++) {
+	for (i = 0; i < VALUES; i++) {
+#pragma HLS PIPELINE
+		c[i] = a[i] + b[i];
+	}
+}
