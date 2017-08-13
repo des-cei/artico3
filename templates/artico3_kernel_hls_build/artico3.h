0bf0aba86914337589fbbd3f622907766ff8a753
Alfonso Rodríguez Medina <alfonsorm89@gmail.com>
2017-08-13 18:15:27 +0000

    artico3.h created online with Bitbucket

---
+++ templates/artico3_kernel_hls_build/artico3.h
@@ -0,0 +1,9 @@
+<a3<artico3_preproc>a3>
+
+#include <stdint.h>
+
+#define A3_KERNEL(<a3<ARGS>a3>) void <a3<NAME>a3>(\
+<a3<generate for PORTS>a3>
+		uint32_t <a3<pid>a3>[<a3<MEMPOS>a3>],\
+<a3<end generate>a3>
+		uint32_t values)
