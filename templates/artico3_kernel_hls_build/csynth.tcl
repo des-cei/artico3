03d8460f742cd3c314415e23d4b98f9b6f3dfd4e
Alfonso Rodriguez <alfonso.rodriguezm@upm.es>
2017-10-19 17:03:16 +0200

    Removed reset generation for all elements, left only for state registers in HLS-based kernel template (config_rtl -reset control)

--- templates/artico3_kernel_hls_build/csynth.tcl
+++ templates/artico3_kernel_hls_build/csynth.tcl
@@ -9,12 +9,13 @@
 
 open_project hls
 set_top <a3<NAME>a3>
-add_files artico3.h
-add_files [ glob *.cpp ]
+#~ add_files artico3.h
+add_files [ glob *.cpp *.c *.h]
 open_solution sol
 set_part {<a3<PART>a3>}
 create_clock -period 10 -name default
-config_rtl -reset all -reset_level low
+#~ config_rtl -reset all -reset_level low
+config_rtl -reset_level low
 source directives.tcl
 csynth_design
 exit
