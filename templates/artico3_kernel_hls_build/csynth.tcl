191889c2c29c660653a824592df36981303878f4
Alfonso Rodriguez <alfonso.rodriguezm@upm.es>
2021-05-10 17:03:26 +0200

    Added dummy testbench file for HLS kernels

--- templates/artico3_kernel_hls_build/csynth.tcl
+++ templates/artico3_kernel_hls_build/csynth.tcl
@@ -7,10 +7,15 @@
 
 <a3<artico3_preproc>a3>
 
+set src [glob *.cpp *.c *.h]
+set idx [lsearch $src "<a3<NAME>a3>_tb.cpp"]
+set src [lreplace $src $idx $idx]
+
 open_project a3_kernel
 set_top <a3<NAME>a3>
 #~ add_files artico3.h
-add_files [ glob *.cpp *.c *.h]
+add_files $src
+add_files -tb <a3<NAME>a3>_tb.cpp
 open_solution sol
 set_part {<a3<PART>a3>}
 create_clock -period 10 -name default
